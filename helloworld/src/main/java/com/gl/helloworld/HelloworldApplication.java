1c89cc145f567494298a6dadda41046c92e763f3
Green Learner <greenlearner01@users.noreply.github.com>
2020-03-17 16:39:22 +0530

    few changes

--- helloworld/src/main/java/com/gl/helloworld/HelloworldApplication.java
+++ helloworld/src/main/java/com/gl/helloworld/HelloworldApplication.java
@@ -1,18 +1,14 @@
 package com.gl.helloworld;
 
-import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HelloworldApplication {
 
     public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(HelloworldApplication.class);
-		app.setBannerMode(Banner.Mode.OFF);
-		app.setWebApplicationType(WebApplicationType.SERVLET);
-		app.run(args);
+        SpringApplication app = new SpringApplication(HelloworldApplication.class);
+        app.run(args);
     }
 
 }
